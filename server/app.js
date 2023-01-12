3c6c956a827bf095f78ed24a507bbb8a41c34931
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-13 00:47:42 +0530

    added auth middleware

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dbConnect from "./config/db.connection.js";
 import { router as productRouter } from "./route/project.route.js";
+import { router as authRouter } from "./route/auth.route.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", productRouter);
+app.use("/api/auth", authRouter);
 
 export default app;
