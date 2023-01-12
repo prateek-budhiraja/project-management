fe66fd73b0ee5b86512dd6200adb37a355362064
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-13 00:46:56 +0530

    added jwt variables

--- server/config/index.js
+++ server/config/index.js
@@ -4,6 +4,9 @@ dotenv.config();
 const config = {
 	PORT: process.env.PORT,
 	MONGO_URI: process.env.MONGO_URI,
+
+	JWT_SECRET: process.env.JWT_SECRET,
+	JWT_EXPIRY: process.env.JWT_EXPIRY,
 };
 
 export default config;
