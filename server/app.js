351fb582f10e28911c753a9eaf55d33200cb93c2
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-13 21:23:38 +0530

    added cookie parser

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dbConnect from "./config/db.connection.js";
+import cookieParser from "cookie-parser";
 import { router as productRouter } from "./route/project.route.js";
 import { router as authRouter } from "./route/auth.route.js";
 
@@ -10,6 +11,7 @@ dbConnect();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api", productRouter);
 app.use("/api/auth", authRouter);
