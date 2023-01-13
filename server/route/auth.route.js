ee1abf5066d756dfb29eb1525f89d6fd61bbc994
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-13 21:26:41 +0530

    added auth route for profile, signup and login with middleware

--- server/route/auth.route.js
+++ server/route/auth.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { home, signup } from "../controller/auth.controller.js";
+import { home, login, profile, signup } from "../controller/auth.controller.js";
+import { isAdmin, isLoggedIn } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.get("/", home);
-router.post("/signup", signup);
+router.post("/signup", isLoggedIn, isAdmin, signup);
+router.post("/login", login);
+router.get("/profile", isLoggedIn, profile);
 
 export { router };
