3b045882445ca1dc50e89f3b2ab572c7795a3835
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-14 15:33:24 +0530

    added add task controller and route

--- server/controller/project.controller.js
+++ server/controller/project.controller.js
@@ -1,7 +1,9 @@
 import Project from "../model/project.model.js";
 import User from "../model/user.model.js";
 import asyncHander from "../service/asyncHandler.js";
-import { PropertyRequiredError } from "../util/customError.js";
+import AuthRole from "../util/authRole.js";
+import { PropertyRequiredError, UnexpectedError } from "../util/customError.js";
+import { TaskStatus } from "../util/status.js";
 
 /**************************************************
  * @HOME
@@ -36,6 +38,49 @@ export const createProject = asyncHander(async (req, res) => {
 		name,
 		lead: req.user,
 	});
+
+	res.status(201).json({
+		success: true,
+		project,
+	});
+});
+
+/**************************************************
+ * @ADD_TASK
+ * @REQUEST_TYPE PATCH
+ * @route http://localhost:4000/api/project/:pid/task/add
+ * @description Add a new task
+ * @parameters name
+ * @returns Project
+ **************************************************/
+
+export const addTask = asyncHander(async (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		throw new PropertyRequiredError("Task Name");
+	}
+
+	const task = {
+		name,
+		assigned_to: req.user,
+		status: TaskStatus.PENDING,
+	};
+
+	if (req.user?.role === AuthRole.ADMIN || req.user?.role === AuthRole.LEAD) {
+		task.status = TaskStatus.APPROVED;
+	}
+
+	const project = await Project.findByIdAndUpdate(
+		req.params.pid,
+		{
+			$push: { tasks: task },
+		},
+		{ new: true }
+	);
+
+	if (!project) {
+		throw new UnexpectedError("Unable to add task");
+	}
 
 	res.status(201).json({
 		success: true,
--- server/route/project.route.js
+++ server/route/project.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createProject, home } from "../controller/project.controller.js";
+import {
+	addTask,
+	createProject,
+	home,
+} from "../controller/project.controller.js";
 import { isLead, isLoggedIn } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.get("/", home);
 router.post("/project/create", isLoggedIn, isLead, createProject);
+router.patch("/project/:pid/task/add", isLoggedIn, addTask);
 
 export { router };
