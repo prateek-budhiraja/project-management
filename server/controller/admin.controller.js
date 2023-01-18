f5524540dbd9ca6cbc9f554dfa18e47bd82441fc
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-18 10:32:08 +0530

    added export user report controller and route

--- server/controller/admin.controller.js
+++ server/controller/admin.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../service/asyncHandler.js";
 import User from "../model/user.model.js";
 import Project from "../model/project.model.js";
+import { TaskStatus } from "../util/status.js";
 import { UnexpectedError } from "../util/customError.js";
 
 import fs from "fs";
@@ -95,6 +96,67 @@ export const exportUsers = asyncHandler(async (_req, res) => {
 				fs.unlinkSync(filePath); // delete this file after 3 minutes
 			}, 60000);
 			return res.download("public/users-" + date + ".csv");
+		}
+	});
+});
+
+/**************************************************
+ * @USER_REPORT
+ * @REQUEST_TYPE GET
+ * @route http://localhost:<PORT>/api/admin/export/users/report
+ * @description Export Users report
+ * @parameters
+ * @returns
+ **************************************************/
+
+export const userReport = asyncHandler(async (_req, res) => {
+	const users = await User.find();
+	const projects = await Project.find();
+	if (!(users || projects)) {
+		throw new UnexpectedError("Enable to export user report");
+	}
+
+	Object.keys(TaskStatus).forEach((status) => (TaskStatus[status] = 0));
+
+	// creating objects for all users
+	var userReport = {};
+	users.forEach((user) => {
+		userReport[JSON.stringify(user._id)] = { ...TaskStatus, email: user.email };
+	});
+
+	// looping through each task of every project and updating values for users
+	projects.forEach((project) => {
+		project.tasks.forEach((task) => {
+			userReport[JSON.stringify(task.assigned_to)][task.status] += 1;
+		});
+	});
+
+	// write to file and send for download
+	const csv = json2csv.parse(userReport);
+	if (!csv) {
+		throw new UnexpectedError("Unable to export projects");
+	}
+
+	let date = new Date();
+	date = `${date.getDate()}-${
+		date.getMonth() + 1
+	}-${date.getFullYear()}-${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+
+	const filePath = path.join(
+		__dirname,
+		"..",
+		"public",
+		"users_report-" + date + ".csv"
+	);
+
+	fs.writeFile(filePath, csv, (err) => {
+		if (err) {
+			throw new UnexpectedError("Unable to export user report");
+		} else {
+			setTimeout(function () {
+				fs.unlinkSync(filePath); // delete this file after 3 minutes
+			}, 60000);
+			return res.download("public/users_report-" + date + ".csv");
 		}
 	});
 });
--- server/route/admin.route.js
+++ server/route/admin.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { exportProjects, exportUsers } from "../controller/admin.controller.js";
+import {
+	exportProjects,
+	exportUsers,
+	userReport,
+} from "../controller/admin.controller.js";
 import { isAdmin, isLoggedIn } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.get("/export/projects", isLoggedIn, isAdmin, exportProjects);
 router.get("/export/users", isLoggedIn, isAdmin, exportUsers);
+router.get("/export/users/report", isLoggedIn, isAdmin, userReport);
 
 export { router };
