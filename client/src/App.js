f0a6474113ae70911960e1e2254a808af3680f64
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-29 22:11:26 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
 	return (
 		<div className="App">
-			<h1 className="text-6xl">Hello</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/dashboard" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
