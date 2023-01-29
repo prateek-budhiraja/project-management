55e6f5635cca4a89b20cbfed1fdd022135bc6493
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-30 02:02:48 +0530

    added axios default config

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
