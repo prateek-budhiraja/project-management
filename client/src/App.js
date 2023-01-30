1531db3dd3a4b1f45324d708141b359cda9a74bd
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-30 19:05:49 +0530

    added toastcontainer

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,23 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
 	return (
 		<div className="App">
+			<ToastContainer
+				position="top-center"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Login />} />
